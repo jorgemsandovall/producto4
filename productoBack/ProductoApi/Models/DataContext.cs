32df96cd329f00921a9e15766ef4f325ea3b73af
Jorge Sandoval <jorge_sandoval-98@hotmail.com>
2019-03-23 13:38:44 -0400

    api con azure

---
+++ productoBack/ProductoApi/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace ProductoApi.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<ProductoApi.Models.product> products { get; set; }
+    }
+}
---
+++ productoBack/ProductoApi/Models/product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductoApi.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Product Name")]
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
